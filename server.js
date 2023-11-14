25bc0d579322196b90f1bbe86fb172779db1e8e1
Carlo0210 <128152855+Carlo0210@users.noreply.github.com>
2023-11-15 06:38:35 +0800

    first

--- server.js
+++ server.js
@@ -12,9 +12,10 @@ const Face   = require('./models/faceModel');
 const multer = require('multer');
 const fs = require('fs-extra');
 const path = require('path');
+const { Canvas, Image, ImageData } = require('canvas');
+const faceapi = require('face-api.js');
+const { createCanvas, loadImage } = require('canvas');
 const bodyParser = require('body-parser');
-const BlazeFace = require('@tensorflow-models/blazeface');
-const tf = require('@tensorflow/tfjs-node');
 
 require('dotenv').config();
 app.use(express.urlencoded({extended: true}));
@@ -35,7 +36,22 @@ const io = require('socket.io')(server, {
   }
 })
 
+// Initialize face-api.js
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
+const faceDetectionNet = faceapi.nets.ssdMobilenetv1;
+const tinyFaceDetector = faceapi.nets.tinyFaceDetector;
+const faceLandmarkNet = faceapi.nets.faceLandmark68Net;
+const faceRecognitionNet = faceapi.nets.faceRecognitionNet;
 
+const loadModels = async () => {
+  const MODEL_PATH = './models'
+  await faceDetectionNet.loadFromDisk(MODEL_PATH);
+  await tinyFaceDetector.loadFromDisk(MODEL_PATH);
+  await faceLandmarkNet.loadFromDisk(MODEL_PATH);
+  await faceRecognitionNet.loadFromDisk(MODEL_PATH);
+};
+loadModels();
+// Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'images/'); // You should create this folder to store uploaded images
@@ -46,26 +62,7 @@ const storage = multer.diskStorage({
 });
 // Set up multer for file uploads
 const upload = multer({storage: storage});
-// Load BlazeFace model
-let blazeFaceModel;
 
-(async () => {
-  blazeFaceModel = await BlazeFace.load();
-})();
-// Middleware to detect faces
-const detectFaces = async (req, res, next) => {
-  try {
-    const imageBuffer = req.file.buffer; // Assuming the image is sent as a file in the request
-    const imageTensor = tf.node.decodeImage(imageBuffer);
-    const faces = await blazeFaceModel.estimateFaces(imageTensor, false);
-
-    req.faces = faces;
-    next();
-  } catch (error) {
-    console.error('Error detecting faces:', error);
-    res.status(500).json({ error: 'Error detecting faces' });
-  }
-};
 
 // Function to calculate the Euclidean distance between two face descriptors
 function euclideanDistance(faceDescriptor1, faceDescriptor2) {
@@ -75,29 +72,108 @@ function euclideanDistance(faceDescriptor1, faceDescriptor2) {
   return Math.sqrt(squaredDistance);
 }
 
-// Route to add face data
-app.post('/post-face', upload.single('image'), detectFaces, async (req, res) => {
+app.post('/post-face', upload.single('image'), async (req, res) => {
   try {
-    const { eventId, name, school, email } = req.body; // Assuming other form data is sent in the request body
+    const { eventId, name, school, email } = req.body;
 
-    // Save the face data to the database
-    const newFace = new FaceDescription({
-      eventId,
-      name,
-      school,
-      email,
-      faceDescription: req.faces,
+    if (!req.file) {
+      return res.status(400).json({ message: 'No image uploaded' });
+    }
+
+    // Load the image from the file path
+    const imagePath = path.join(__dirname, req.file.path);
+    const image = await loadImage(imagePath);
+    const canvas = createCanvas(image.width, image.height);
+    const context = canvas.getContext('2d');
+    context.drawImage(image, 0, 0);
+
+    let fullFaceDescriptions = await faceapi.detectAllFaces(canvas).withFaceLandmarks().withFaceDescriptors();
+
+    if (fullFaceDescriptions.length === 0) {
+      return res.status(400).json({ message: 'No face detected in the image' });
+    }
+
+    fullFaceDescriptions = faceapi.resizeResults(fullFaceDescriptions, { width: image.width, height: image.height });
+
+    // Check if a similar face already exists in the database
+    const existingFaces = await Face.find(); // Assuming you have a Face model defined
+
+    let isDuplicateFaceDescription = false;
+    let isDuplicateEmail = false;
+
+    for (const newFaceDescription of fullFaceDescriptions) {
+      const newFaceDescriptor = newFaceDescription.descriptor;
+
+      for (const existingFace of existingFaces) {
+        // Check if the eventId and email match before comparing faces
+        if (existingFace.eventId === eventId) {
+          // Check for duplicate face descriptions
+          const existingFaceDescriptors = existingFace.faceDescription.map((faceData) => faceData.faceDescriptor);
+
+          for (const existingFaceDescriptor of existingFaceDescriptors) {
+            const distance = euclideanDistance(newFaceDescriptor, existingFaceDescriptor);
+
+            // You can set a threshold for similarity, e.g., 0.6 (adjust as needed)
+            if (distance < 0.6) {
+              isDuplicateFaceDescription = true;
+              break;
+            }
+          }
+
+          if (isDuplicateFaceDescription) {
+            break;
+          }
+        }
+
+        // Check for duplicate email
+        if (existingFace.email === email) {
+          isDuplicateEmail = true;
+          break;
+        }
+      }
+
+      if (isDuplicateFaceDescription) {
+        return res.status(400).json({ message: 'This face is already registered on this event.' });
+      }
+      if (isDuplicateEmail) {
+        return res.status(400).json({ message: 'This email is already exist. Try another different email address.' });
+      }
+    }
+    // Save data to MongoDB, including faceDescriptions and distances
+    const facesData = fullFaceDescriptions.map((faceDescription) => {
+      const { x, y, width, height } = faceDescription.detection.box;
+      const faceBox = { x, y, width, height };
+      const faceDescriptor = faceDescription.descriptor;
+      const faceLandmarks = faceDescription.landmarks;
+
+      // Calculate the distances between this face and all other faces
+      const distances = fullFaceDescriptions.map((otherFaceDescription) => {
+        const distance = euclideanDistance(faceDescriptor, otherFaceDescription.descriptor);
+        return distance;
+      });
+
+      return {
+        faceBox,
+        faceDescriptor,
+        faceLandmarks,
+        distances,
+      };
     });
 
+
+
+    const newFace = new Face({ eventId, name, school, email, faceDescription: facesData });
     await newFace.save();
 
-    res.status(200).json({ message: 'Face data saved successfully' });
+    // Remove the uploaded image
+    await fs.promises.unlink(req.file.path);
+
+    res.status(201).json({ message: 'Face added successfully' });
   } catch (error) {
-    console.error('Error saving face data:', error);
-    res.status(500).json({ error: 'Error saving face data' });
+    console.error(error);
+    res.status(500).json({ message: 'Internal Server Error' });
   }
 });
-
 
 
 app.post('/compare-faces', upload.single('image'), async (req, res) => {
