3eae645b666b54e8c5e5b0b75dbce766ce1b229c
Carlo0210 <128152855+Carlo0210@users.noreply.github.com>
2023-11-15 04:15:47 +0800

    first

--- routes/faceRoutes.js
+++ routes/faceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Face = require('./models/faceModel'); // Assuming faceModel.js is in the same directory
+const Face = require('../models/faceModel'); // Assuming faceModel.js is in the same directory
 const multer = require('multer');
 const fs = require('fs-extra');
 const path = require('path');
