e155e70c0df9ae974cb1c09efddc2487b796a77f
Carlo0210 <128152855+Carlo0210@users.noreply.github.com>
2023-12-07 12:25:08 +0800

    first

--- server.js
+++ server.js
@@ -257,7 +257,15 @@ app.post('/compare-faces', upload.single('image'), async (req, res) => {
 });
 
 
-
+// Endpoint to get all faces
+app.get('/faces', async (req, res) => {
+  try {
+    const faces = await Face.find();
+    res.json(faces);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 
 
